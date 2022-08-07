9c8fa95c34e859e17f60a06552c53abd7c1127d3
jmayorga94 <javi.mayorga1994@outlook.com>
2022-08-06 22:28:10 -0600

    Adds: hands on aws instance

---
+++ 1.Fundamentals/HandsOn/aws_ubuntu_instance/main.tf
@@ -0,0 +1,13 @@
+#Deploy a single server
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-02f3416038bdb17fb"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0f9c3ced536139c0c"
+
+
+}
