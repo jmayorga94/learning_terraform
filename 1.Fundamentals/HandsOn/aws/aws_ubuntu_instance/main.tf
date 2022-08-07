40682b1357aa6706d7c7c0577ee98811e66fffcf
jmayorga94 <javi.mayorga1994@outlook.com>
2022-08-06 22:31:36 -0600

    adds: deploys with tags

--- 1.Fundamentals/HandsOn/aws/aws_ubuntu_instance/main.tf
+++ 1.Fundamentals/HandsOn/aws/aws_ubuntu_instance/main.tf
@@ -8,6 +8,8 @@ resource "aws_instance" "example" {
   ami = "ami-02f3416038bdb17fb"
   instance_type = "t2.micro"
   subnet_id = "subnet-0f9c3ced536139c0c"
-
+  tags={
+    Name= "terraform.example"
+  }
 
 }
